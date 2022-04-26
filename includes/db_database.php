343be6d852097dcf36413fe8d66e34881cd01fb4
sysbayt <sysbayt@gmail.com>
2022-04-26 14:54:54 -0500

    set local db

--- includes/db_database.php
+++ includes/db_database.php
@@ -2,21 +2,21 @@
   class MySQL {
     private $conexion;
     public function __construct() {
-        /* $host   = "localhost";
+        $host   = "localhost";
         $dbname = "erp";
         $user   = "root";
         $pass   = "";
- */
+ 
         /* $host   = "localhost";
         $dbname = "kevivphl_sb_erp";
         $user   = "kevivphl_ksanchezgo";
-        $pass   = "Kanchez123"; */
+        $pass   = "Kanchez123";
 
         //heroku_2
         $host   = "kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         $dbname = "isg0f26bvkz76vjd";
         $user   = "x0obcpfmbmeoh6g2";
-        $pass   = "jjvuwi069kl9bqid";
+        $pass   = "jjvuwi069kl9bqid";*/
         if (!isset($this->conexion)) {
             $this->conexion = (mysqli_connect($host,$user,$pass,$dbname)) or die(mysqli_error());
         }
